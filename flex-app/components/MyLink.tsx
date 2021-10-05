97054361c22c9a880a24a57c9398b8e2e53dbda6
ＴＲ <public@technotter.com>
2021-10-05 15:37:10 +0900

    ページリンクを楽に張れるようにコンポーネントを追加

---
+++ flex-app/components/MyLink.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import { ReactNode } from 'react';
+import * as style from '../styles/MyLink.styles'
+
+const MyLink = (props: { children?: ReactNode, href?: string }) => (
+  <Link href="/">
+    <a style={style.link}>{props.children}</a>
+  </Link>
+);
+
+export default MyLink;
