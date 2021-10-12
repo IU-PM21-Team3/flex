cde613cb2ac0e5405f565f99ba3d41fc004e5ff4
nishi6400 <91527656+nishi6400@users.noreply.github.com>
2021-10-12 11:01:49 +0900

    google-map

---
+++ flex-app/components/Google_Map.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "400px",
+  height: "400px",
+};
+
+const center = {
+  lat: 35.69575,
+  lng: 139.77521,
+};
+
+function G_Map () {
+  return (
+    <LoadScript googleMapsApiKey="AIzaSyD5hEtmrnaidWTm_VEVo0Qq6lmgV4WyWKQ">
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={17}
+      ></GoogleMap>
+    </LoadScript>
+  );
+};
+
+export default G_Map;
