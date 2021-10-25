78fcda9c63e807bb09b5ba8dee9dfa3f6b50a2fe
ＴＲ <public@technotter.com>
2021-10-25 23:15:26 +0900

    キャッシュ機能を削除

--- flex-functions/src/GetTrafficInfo.server.ts
+++ flex-functions/src/GetTrafficInfo.server.ts
@@ -15,7 +15,6 @@ export const API_KEY_OPT: IRequestOptions = {
     }
   }
 };
-const MINIMUM_VALID_TIME = new Date(0, 0, 0, 0, 15, 0).getTime();
 // #endregion
 
 /** 運行情報を取得する関数 */
@@ -24,24 +23,5 @@ export const getTrafficInfo = functions.https.onRequest(async (request, response
   const res: IRestResponse<IOdptTrainInformation[]>
     = await rest.get<IOdptTrainInformation[]>("/api/v4/" + TRAFFIC_INFO_KEY, API_KEY_OPT);
 
-  let minimumValidTime: Date = new Date(Date.now() + MINIMUM_VALID_TIME);
-  res.result?.forEach(e => {
-    // 一旦データ保証期限情報をキャッシュする
-    const validT = e["dct:valid"];
 
-    // データ保証期限情報が存在し, かつ参照中の期限情報の方が早く到来する場合には, 
-    // 暫定期限情報を更新する
-    if (validT != null && validT < minimumValidTime)
-      minimumValidTime = validT;
-  });
-
-  // 何秒間キャッシュしても大丈夫かを計算する
-  const DATE_NOW: number = Date.now();
-  const VALID_LIMIT: number = minimumValidTime.getTime();
-
-  // キャッシュの生存期間 [秒]
-  const CACHE_LIFE_S: number = Math.floor((VALID_LIMIT - DATE_NOW) / 1000);
-
-  // cache ref : https://blog.katsubemakito.net/firebase/cloudfunctions-cdn-cache
-  response.set("Cache-Control", "public, max-age=" + CACHE_LIFE_S).send(res);
 });
