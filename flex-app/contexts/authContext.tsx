00c535250dfe6f824b32eca6fe55a4f2dc2b6088
ＴＲ <public@technotter.com>
2021-11-02 06:22:37 +0900

    ログイン/登録用UIを実装
    Routingが上手く動かないため要改修

---
+++ flex-app/contexts/authContext.tsx
@@ -0,0 +1,29 @@
+import { createContext, useState, useContext, useEffect, ReactNode } from "react";
+import { User } from "firebase/auth";
+import flexFirebase from "../firebase/clientApp"
+
+interface IContainsUser {
+  user: User | null;
+}
+
+const AuthContext = createContext<IContainsUser>({ user: null });
+
+export function GetAuthContext() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }: { children: ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribed = flexFirebase.auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+
+    return () => {
+      unsubscribed();
+    };
+  }, []);
+
+  return <AuthContext.Provider value={{ user: user }}>{children}</AuthContext.Provider>;
+}
--- flex-app/pages/_app.tsx
+++ flex-app/pages/_app.tsx
@@ -1,7 +1,49 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import NavigationBar from "../components/NavigationBar";
+import { createMemoryHistory } from "history";
+import { AuthProvider, GetAuthContext } from "../contexts/authContext";
+
+//#region Pages
+import booking from "./booking";
+import community_home from "./community_home";
+import community_talk from "./community_talk";
+import index from "./index";
+import paidPlan from "./paidPlan";
+import timeLine from "./timeLine";
+import uploadPhoto from "./uploadPhoto";
+//#endregion
+
+const myHistory = createMemoryHistory();
+
+const PrivateRoute = ({ component, exact, path }: { component: any, exact?: boolean, path: string }) => {
+  const { user } = GetAuthContext();
+  return user ? (
+    <Route exact={exact} path={path} component={component} />
+  ) : (
+    <Redirect to="/" />
+  );
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Router history={myHistory}>
+        <NavigationBar />
+
+        <Switch>
+          <Route path="/" exact component={index} />
+
+          <Route path="/booking" component={booking} />
+          <Route path="/community_home" component={community_home} />
+          <Route path="/community_talk" component={community_talk} />
+          <Route path="/paidPlan" component={paidPlan} />
+          <Route path="/timeLine" component={timeLine} />
+          <Route path="/uploadPhoto" component={uploadPhoto} />
+        </Switch>
+      </Router>
+    </AuthProvider>
+  )
 }
 export default MyApp
