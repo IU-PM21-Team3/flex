30298d9cc2e19ac0e19632d7d4f7e1ba16b79410
ＴＲ <public@technotter.com>
2021-11-05 20:47:56 +0900

    不要なファイルを削除

--- flex-app/jest.config.ts
+++
@@ -1,9 +0,0 @@
-/* eslint import/no-anonymous-default-export: 0 */
-export default {
-  clearMocks: true,
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
-  testEnvironment: "node",
-  testPathIgnorePatterns: ["/node_modules/", "/src/", "/public/", "/build/"],
-};
