44c315ad70f50185658213b03b66d0c43c546086
ＴＲ <public@technotter.com>
2021-11-06 05:34:49 +0900

    flex-appでCloud Functionsを使用するため, functionsのファイルをflex-app側に移動

--- flex-functions/src/index.ts
+++
@@ -1,7 +0,0 @@
-// import { getTrafficInfo } from "./GetTrafficInfo.server";
-import * as AuthOnCreateDelete from "./AuthOnCreateDelete";
-
-// export const gettrafficinfo = getTrafficInfo;
-
-export const CreateNewFirestoreDocumentForNewUser = AuthOnCreateDelete.CreateNewFirestoreDocumentForNewUser;
-export const DeleteFirestoreDocumentForWithdrawUser = AuthOnCreateDelete.DeleteFirestoreDocumentForWithdrawUser;
