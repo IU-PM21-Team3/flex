49f45ec76ac620bfea421b448ee29c09ded6ae20
nishi6400 <91527656+nishi6400@users.noreply.github.com>
2021-11-09 12:01:47 +0900

    ClosedInfo

--- flex-app/components/Closed_Info.tsx
+++ flex-app/components/Closed_Info.tsx
@@ -1,29 +1,68 @@
 import React, { useState, useEffect } from "react";
 import { Marker, InfoWindow } from "@react-google-maps/api";
 import { stringify } from "querystring";
+import axios, { AxiosResponse } from "axios";
 
-interface GMapWindow extends Window {
-  google: any;
-}
-declare const window: GMapWindow;
-interface GMapWindow extends Window {
-  google: any;
-}
-declare const google: GMapWindow;
-
-var axios = require("axios");
 var business_status: any = "";
 
-async function Get_buisiness_stats(placeID: string){
-  var config = {
-    method: "get",
-    url: `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeID}&fields=name%2Crating%2Cformatted_phone_number%2Cbusiness_status&key=AIzaSyD5hEtmrnaidWTm_VEVo0Qq6lmgV4WyWKQ`,
-    headers: {},
-  };
-  
-  return await axios(config)
+export async function fetch_all_place_data(/*placeID: string*/) {
+  return await axios
+    .get(
+      `https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJCewJkL2LGGAR3Qmk0vCTGkg&fields=geometry%2Cformatted_address%2Cname%2Crating%2Cformatted_phone_number%2Cbusiness_status&key=AIzaSyD5hEtmrnaidWTm_VEVo0Qq6lmgV4WyWKQ`
+    )
     .then(function (response: any) {
-      business_status = /"business_status" : (.+)/.exec(JSON.stringify(response.data))[1];
+      const result = response.data;
+      if (result == null) return null;
+      else return result;
+    })
+    .catch(function (error: any) {
+      console.log(error);
+    });
+}
+
+export async function Fetch_Lat(/*placeID: string*/) {
+  return await axios
+    .get(
+      `https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJCewJkL2LGGAR3Qmk0vCTGkg&fields=geometry%2Cformatted_address%2Cname%2Crating%2Cformatted_phone_number%2Cbusiness_status&key=AIzaSyD5hEtmrnaidWTm_VEVo0Qq6lmgV4WyWKQ`
+    )
+    .then(function (response: any) {
+      const result = response.data;
+      let lat: number;
+      if (result == null) return null;
+      else lat = result.result.geometry.location.lat;
+      return lat;
+    })
+    .catch(function (error: any) {
+      console.log(error);
+    });
+}
+
+export async function Fetch_Lng(/*placeID: string*/) {
+  return await axios
+    .get(
+      `https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJCewJkL2LGGAR3Qmk0vCTGkg&fields=geometry%2Cformatted_address%2Cname%2Crating%2Cformatted_phone_number%2Cbusiness_status&key=AIzaSyD5hEtmrnaidWTm_VEVo0Qq6lmgV4WyWKQ`
+    )
+    .then(function (response: any) {
+      const result = response.data;
+      let lng: number;
+      if (result == null) return null;
+      else lng = result.result.geometry.location.lng;
+      return lng;
+    })
+    .catch(function (error: any) {
+      console.log(error);
+    });
+}
+  
+export async function Get_buisiness_stats(placeID: string) {
+  return await axios
+    .get(
+      `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeID}&fields=name%2Crating%2Cformatted_phone_number%2Cbusiness_status&key=AIzaSyD5hEtmrnaidWTm_VEVo0Qq6lmgV4WyWKQ`
+    )
+    .then(function (response: any) {
+      const result = response.data;
+      if (result == null) return null;
+      else business_status = result.result.business_status;
       return business_status;
     })
     .catch(function (error: any) {
@@ -31,57 +70,29 @@ async function Get_buisiness_stats(placeID: string){
     });
 }
 
-export default function PlaceInfo() {
-  const places = [
-    { info: "あいうえお", location: { lat: 35.69575, lng: 139.77521 } },
-  ];
+function num1() {
+  return 35.6585805
+}
 
+function num2() {
+  return 139.7454329
+}
+
+// ****************************************
+// ***** 開発中 ***************************
+// 地図上にマーカーをつけて表示するためのもの
+
+export function ClosedInfo() {
   const [selected, setSelected] = useState(null);
 
-  let buisiness_status: any;
-  let placeID: string
+  let x = Fetch_Lat();
+  let y = Fetch_Lng();
 
-  useEffect(() => {
-    buisiness_status = Get_buisiness_stats(placeID)
-  });
-
-  return ( 
-    <>
-      {places.map((marker) => (
-        <Marker
-          key={`${marker.location.lat * marker.location.lng}`}
-          position={{
-            lat: marker.location.lat,
-            lng: marker.location.lng,
-          }}
-          onMouseOver={() => {
-            setSelected(marker);
-            // マウスオーバーで<InfoWindow>が描画されます。
-          }}
-          icon={{
-            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT30_Pzks-acC6IR3VZ6L3vBugTbOl_1u_wTQ&usqp=CAU",
-            origin: new window.google.maps.Point(0, 0),
-            anchor: new window.google.maps.Point(15, 15),
-            scaledSize: new window.google.maps.Size(30, 30),
-            // ここでアイコン表示の設定ができます。
-          }}
-        />
-      ))}
-
-      {selected ? (
-        // MarkerにマウスオーバーされたときにInfoWindowが表示されます。
-        <InfoWindow
-          position={{
-            lat: selected.location.lat,
-            lng: selected.location.lng,
-          }}
-          onCloseClick={() => {
-            setSelected(null);
-          }}
-        >
-          <div>{selected.info}</div>
-        </InfoWindow>
-      ) : null}
-    </>
+  const b = { lat: 35.6585805, lng: 139.7454329 };
+  
+  return (
+    
+      <Marker position={b} />
+    
   );
 }
