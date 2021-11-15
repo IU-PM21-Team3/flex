56410e2dcac4c13d59e846ca1a12f705e2e346ce
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2021-11-16 00:34:29 +0900

    コンフリクト修正ミスを修正

--- flex-app/pages/timeLine.tsx
+++ flex-app/pages/timeLine.tsx
@@ -6,23 +6,15 @@ import { DBTravelPlanSummary } from "../firebase/DBTypes";
 import { doc } from "firebase/firestore";
 import app from "../firebase/clientApp";
 
-// 旅程のテーマと日付の設定
-type travelPlanProps = {
-  planName: string,
-  // description: string,
-  beginDate: Date,
-  endDate: Date,
-  // lastUpdate: Date
-  // planDoc: Reference
-};
 
 // 初期値
-const travelPlanProps: travelPlanProps = {
+const travelPlanProps: DBTravelPlanSummary = {
   planName: "福島-茨城旅行",
   // Dateの「月」指定は 0 ~ 11 である点に注意
   beginDate: new Date( 2021, 11, 30 ),
   endDate: new Date(2021, 12, 1),
   lastUpdate: new Date(),
+  description: "テスト用データ",
   planDoc: doc(app.store, "/travelPlans/testData")
 };
 
