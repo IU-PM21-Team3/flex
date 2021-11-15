457e359cdfd2e870b609980fb441ab0bd137e221
uai <noema01@outlook.jp>
2021-11-15 22:55:41 +0900

    GMapの修正

--- flex-app/components/Google_Map.tsx
+++ flex-app/components/Google_Map.tsx
@@ -1,26 +1,76 @@
-import React from "react";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import React, { useState, useEffect } from "react";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
-const containerStyle = {
+type Pos = {
+  lat: number;
+  lng: number;
+};
+
+type GMapProps = {
+  isMarkerShown?: boolean;
+  containerSize?: {
+    width: string;
+    height: string;
+  };
+  center?: Pos;
+  setLatLng?: React.Dispatch<React.SetStateAction<Pos | undefined>>;
+};
+
+const defaultContainerSize = {
   width: "600px",
   height: "400px",
 };
 
-const center = {
-  lat: 35.69575,
-  lng: 139.77521,
+// 東京駅
+const defaultCenter = {
+  lat: 35.6812362,
+  lng: 139.7649361,
 };
 
-function G_Map() {
+
+const GMap = (props: GMapProps) => {
+  const [center, setCenter] = useState<Pos>();
+  const [containerSize, setContainerSize] = useState<any>();
+
+  useEffect(() => {
+    if (props.containerSize) {
+      setContainerSize(props.containerSize);
+    } else {
+      setContainerSize(defaultContainerSize);
+    }
+
+    if (props.center) {
+      setCenter(props.center);
+    } else {
+      setCenter(defaultCenter);
+    }
+  }, []);
+
+  // 地図をクリックしたときの座標を取得する
+  const getLatLngByClick = (event: google.maps.MapMouseEvent) => {
+    // const resLatLng = event.latLng?.toString();
+    const resLat = event.latLng?.lat();
+    const resLng = event.latLng?.lng();
+    if (resLat && resLng && props.setLatLng) {
+      props.setLatLng({
+        lat: resLat,
+        lng: resLng
+      });
+    }
+  };
+
   return (
     <LoadScript googleMapsApiKey="AIzaSyD5hEtmrnaidWTm_VEVo0Qq6lmgV4WyWKQ">
       <GoogleMap
-        mapContainerStyle={containerStyle}
+        mapContainerStyle={{ ...containerSize }}
         center={center}
         zoom={13}
-      ></GoogleMap>
+        onClick={getLatLngByClick}
+      >
+        {props.isMarkerShown && <Marker position={center ? center : defaultCenter} />}
+      </GoogleMap>
     </LoadScript>
   );
-}
+};
 
-export default G_Map;
+export default GMap;
--- flex-app/pages/create.tsx
+++ flex-app/pages/create.tsx
@@ -1,18 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { NextPage } from "next";
 import PrivatePage from "../components/PrivatePage";
-import { formatDate, fetchTodayDate, fetchNextDate } from "../utils/utils";
 import styles from "../styles/CreatePage.module.css";
-import G_Map from "../components/Google_Map";
-type travelPlanSummary = {
+import GMap from "../components/Google_Map";
+import { Grid } from "@material-ui/core";
+
+
+type Pos = {
+  lat: number;
+  lng: number;
+};
+type TravelPlanSummary = {
   planName: string;
   description: string;
   beginDate: Date;
   endDate: Date;
-  initialPlace: {
-    lat: string;
-    lng: string;
-  };
+  initialPlace: Pos;
+  origin: Pos;
+  destination: Pos;
 };
 
 
@@ -21,78 +26,95 @@ const formatDateStr = "yyyy-MM-dd";
 const CreatePlanPage: NextPage = () => {
   const [planName, setPlanName] = useState("");
   const [desc, setDesc] = useState("");
-  const [beginDate, setBeginDate] = useState(formatDate(fetchTodayDate(), formatDateStr));
-  const [endDate, setEndDate] = useState(formatDate(fetchNextDate(fetchTodayDate()), formatDateStr));
-  const [lat, setLat] = useState(0.0);
-  const [lng, setLng] = useState(0.0);
+  const [beginDate, setBeginDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [center, setCenter] = useState<Pos>();
+  const [origin, setOrigin] = useState<Pos>();
+
+  useEffect(() => {
+    if (origin) {
+      setCenter(origin);
+    }
+  }, [origin]);
 
   // データを一つにまとめる
   const createTravelPlanSummayData = () => {
-    console.log("create!!");
     return;
   };
 
+  // 作成ボタンを押した時の処理
   const submit = (e: any) => {
     e.preventDefault();
     createTravelPlanSummayData();
     // reset
     setPlanName("");
     setDesc("");
-    setBeginDate(formatDate(fetchTodayDate(), formatDateStr));
-    setEndDate(formatDate(fetchNextDate(fetchTodayDate()), formatDateStr));
+    setBeginDate("");
+    setEndDate("");
   };
 
   return (
     <PrivatePage>
-      <div className={styles.c_wrap}>
-        <h1>旅程プランの作成</h1>
-        <form onSubmit={submit}>
-          <label className="required" htmlFor="planName">旅程のテーマ</label>
-          <input
-            value={planName}
-            onChange={(e) => setPlanName(e.target.value)}
-            type="text"
-            name="planName"
-            placeholder="例）限界旅行"
-            required
-            aria-required="true"
-          />
+      <h1 className="pageHeadText">旅程プランの作成</h1>
+      <Grid container text-alignItems="center">
+        <Grid item xs={4}>
+          <form onSubmit={submit}>
+            <label className="required" htmlFor="planName">旅程のテーマ</label>
+            <input
+              value={planName}
+              onChange={(e) => setPlanName(e.target.value)}
+              type="text"
+              name="planName"
+              placeholder="例）限界旅行"
+              required
+              aria-required="true"
+            />
 
-          <label htmlFor="description">概要</label>
-          <textarea
-            value={desc}
-            onChange={(e) => setDesc(e.target.value)}
-            name="description"
-            placeholder="時間ギリギリを攻める"
-            rows={5}
-            cols={45}
-          />
+            <label htmlFor="description">概要</label>
+            <textarea
+              value={desc}
+              onChange={(e) => setDesc(e.target.value)}
+              name="description"
+              placeholder="例）時間ギリギリを攻める"
+              rows={5}
+              cols={45}
+            />
 
-          <label className="required" htmlFor="beginDate">開始日時</label>
-          <input
-            value={beginDate}
-            onChange={(e) => setBeginDate(e.target.value)}
-            type="date"
-            name="beginDate"
-            required
-            aria-required="true"
-          />
+            <label className="required" htmlFor="beginDate">開始日</label>
+            <input
+              value={beginDate}
+              onChange={(e) => setBeginDate(e.target.value)}
+              type="date"
+              name="beginDate"
+              required
+              aria-required="true"
+            />
 
-          <label className="required" htmlFor="endDate">終了日時</label>
-          <input
-            value={endDate}
-            onChange={(e) => setEndDate(e.target.value)}
-            type="date"
-            name="endDate"
-            required
-            aria-required="true"
-          />
+            <label className="required" htmlFor="endDate">終了日</label>
+            <input
+              value={endDate}
+              onChange={(e) => setEndDate(e.target.value)}
+              type="date"
+              name="endDate"
+              required
+              aria-required="true"
 
-          <label>目的地周辺を指定してください</label>
-          <G_Map />
-          <button className={styles.c_btn}>旅程プランを作成する</button>
-        </form>
-      </div>
+              style={{ marginBottom: "20px" }}
+            />
+
+            <button className={styles.c_btn}>旅程プランを作成する</button>
+          </form>
+        </Grid>
+
+        <Grid item xs={8}>
+          <GMap
+            isMarkerShown
+            containerSize={{ width: "100%", height: "70vh" }}
+            center={center}
+            setLatLng={setOrigin}
+          />
+        </Grid>
+      </Grid>
     </PrivatePage>
   );
 };
