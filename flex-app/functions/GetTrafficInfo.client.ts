21af5b362c360e6ef6fe6fde5bfbd977db63b5a6
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2021-11-18 22:37:20 +0900

    旅行プランの作成および修正テストを実装

--- flex-app/functions/GetTrafficInfo.client.ts
+++ flex-app/functions/GetTrafficInfo.client.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { IOdptTrainInformation, /* fromIOdptTrainInformationArrWithString */ } from "./GetTrafficInfo.type";
 
 const API_KEY = "eeb79adabbb87d6aefdd93ebbde513a95cd99e6634405c84fa99fbbb060a671f";
@@ -5,5 +6,5 @@ const TRAFFIC_INFO_KEY = "odpt:TrainInformation";
 const BASE_URL = "https://api.odpt.org";
 
 export async function getTrafficInfo(): Promise<IOdptTrainInformation<Date>[] | null> {
-  return fetch(BASE_URL + "/api/v4/" + TRAFFIC_INFO_KEY + "?acl:consumerKey=" + API_KEY).then((v) => v.json() as unknown as IOdptTrainInformation<Date>[]);
+  return axios.get<IOdptTrainInformation<Date>[]>(BASE_URL + "/api/v4/" + TRAFFIC_INFO_KEY + "?acl:consumerKey=" + API_KEY).then((v) => v.data);
 }
