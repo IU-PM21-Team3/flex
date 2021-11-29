01be4a46b95b05befab450e98acbd92e226e3428
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2021-11-30 02:33:36 +0900

    旅行プランを編集するためのクラスを作成

---
+++ flex-app/firebase/DBTravelPlanDataCtrler.ts
@@ -0,0 +1,59 @@
+import { updatedDiff } from "deep-object-diff";
+import { cloneDeep } from "lodash";
+import { DBTravelPlanSummary } from "./DBTypes";
+import { TravelPlanController } from "./TravelPlanController";
+
+export class DBTravelPlanDataCtrler {
+  private _travelPlanCtrler: TravelPlanController;
+  public get travelPlanCtrler(): TravelPlanController {
+    return this._travelPlanCtrler;
+  }
+
+  private _DBTravelPlanSummary: DBTravelPlanSummary;
+  public get DBTravelPlanSummary(): DBTravelPlanSummary {
+    return this._DBTravelPlanSummary;
+  }
+
+  private _OriginalDBTravelPlanSummary: DBTravelPlanSummary;
+  public get OriginalDBTravelPlanSummary(): Readonly<DBTravelPlanSummary> {
+    return this._OriginalDBTravelPlanSummary;
+  }
+
+  private _isDeleted = true;
+  public get isDeleted(): boolean {
+    return this._isDeleted;
+  }
+
+  private readonly planID: string;
+
+  public get date(): Date {
+    return new Date(this._date);
+  }
+  private readonly _date: Date;
+
+  constructor(ctrler: TravelPlanController, planID: string, date: Date, dbTravelPlanSummary: DBTravelPlanSummary) {
+    this._travelPlanCtrler = ctrler;
+    this.planID = planID;
+    this._date = date;
+    this._DBTravelPlanSummary = dbTravelPlanSummary;
+    this._OriginalDBTravelPlanSummary = cloneDeep(dbTravelPlanSummary);
+  }
+
+  public updateTravelPlanSummary(newData?: DBTravelPlanSummary): Promise<DBTravelPlanSummary> {
+    if (newData != null) {
+      this._DBTravelPlanSummary = newData;
+    }
+
+    return this.travelPlanCtrler
+      .updatePlanSummary(this.planID, updatedDiff(this.OriginalDBTravelPlanSummary, this.DBTravelPlanSummary))
+      .then(() => this._OriginalDBTravelPlanSummary = cloneDeep(this.DBTravelPlanSummary));
+  }
+
+  public addReadableWritableUsers(readableUserID?: string | string[], writableUserID?: string | string[]) {
+    return this.travelPlanCtrler.addTravelPlanRWableUser(this.planID, readableUserID, writableUserID);
+  }
+
+  public getTravelPlan() {
+    return this.travelPlanCtrler.getTravelPlan(this.planID);
+  }
+}
