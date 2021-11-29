4f100344dcda03d31991111afe506c3db740a983
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2021-11-30 02:27:10 +0900

    Create TravelPlanRefUpdater
    旅行プランの新規追加時, およびアクセス可能ユーザ修正時や削除時等に, ユーザデータ側にも変更を反映する関数を実装

---
+++ flex-app/functions/TravelPlanRefUpdater.ts
@@ -0,0 +1,60 @@
+import { firestore } from "firebase-admin";
+import { FieldValue } from "firebase-admin/firestore";
+import { DBTravelPlan, DBUser } from "../firebase/DBTypes";
+import { DocumentReference } from "@firebase/firestore";
+import { document } from "firebase-functions/v1/firestore";
+import { firebase } from "./firebaseApp";
+
+function getReadableUsersRefArr(d:DBTravelPlan):Array<DocumentReference<DBUser>> {
+  return "readableUsers" in d ? d.readableUsers : [];
+}
+
+function getWritableUsersRefArr(d: DBTravelPlan): Array<DocumentReference<DBUser>> {
+  return "writableUsers" in d ? d.writableUsers : [];
+}
+function getUsersIDArr(d?: DBTravelPlan): Array<string> {
+  if (d == undefined) {
+    return [];
+  } else {
+    return Array.from(
+      new Set(
+        getReadableUsersRefArr(d)
+          .concat(
+            getWritableUsersRefArr(d)
+          )
+      )
+    ).map((v) => v.id);
+  }
+}
+
+export const OnTravelPlanWritten = document("travelPlans/{planID}").onWrite((change) => {
+  const newAccessableUsers = getUsersIDArr(change.after.data() as DBTravelPlan);
+  const oldAccessableUsers = getUsersIDArr(change.before.data() as DBTravelPlan);
+
+  // 新規追加ユーザを検出する
+  newAccessableUsers.forEach((userID) => {
+    // 変更前にも存在したのであれば, 新規追加ユーザではない
+    if (oldAccessableUsers.indexOf(userID) >= 0) {
+      return;
+    }
+
+
+    // ユーザデータにTravelPlanのRefを追加する
+    return firestore(firebase).doc("/users/" + userID).update({
+      planSummaries: FieldValue.arrayUnion(change.after.ref)
+    });
+  });
+
+  // 削除ユーザを検出する
+  oldAccessableUsers.forEach((userID) => {
+    // 変更前にも存在したのであれば, 削除されたユーザではない.
+    if (newAccessableUsers.indexOf(userID) >= 0) {
+      return;
+    }
+
+    // ユーザデータからTravelPlanのRefを削除する
+    return firestore(firebase).doc("/users/" + userID).update({
+      planSummaries: FieldValue.arrayRemove(change.after.ref)
+    });
+  });
+});
--- flex-app/functions/index.ts
+++ flex-app/functions/index.ts
@@ -1,7 +1,8 @@
 import * as NextJSFunctions from "./firebaseFunctions";
 import * as _AuthOnCreateDelete from "./AuthOnCreateDelete";
+import * as _TravelPlanRefUpdater from "./TravelPlanRefUpdater";
 
 export const nextjsFunc = NextJSFunctions.nextjsFunc;
 
-
 export const AuthOnCreateDelete = _AuthOnCreateDelete;
+export const TravelPlanRefUpdater = _TravelPlanRefUpdater;
