9edb44033dc8092107ee4049fa5c29fce0773d09
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2021-11-30 01:23:02 +0900

    不要なコメントを削除

--- flex-app/functions/index.ts
+++ flex-app/functions/index.ts
@@ -1,10 +1,8 @@
 import * as NextJSFunctions from "./firebaseFunctions";
-// import { getTrafficInfo } from "./GetTrafficInfo.server";
 import * as AuthOnCreateDelete from "./AuthOnCreateDelete";
 
 export const nextjsFunc = NextJSFunctions.nextjsFunc;
 
-// export const gettrafficinfo = getTrafficInfo;
 
 export const CreateNewFirestoreDocumentForNewUser = AuthOnCreateDelete.CreateNewFirestoreDocumentForNewUser;
 export const DeleteFirestoreDocumentForWithdrawUser = AuthOnCreateDelete.DeleteFirestoreDocumentForWithdrawUser;
